35c577df63a5c02b389b015c144b5bc93d55055c
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-04-23 07:00:26 +0100

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+""" ALX interview 1 - Pascal Triangle """
+
+
+def pascal_triangle(n):
+    """_summary_
+
+    Args:
+        n (int): integer to convert to Pascal Triangle
+
+    Returns:
+        list: a list containing Pascal Triangle of int n
+    """
+    if n <= 0:
+        return []
+
+    P_triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(P_triangle[i-1][j-1] + P_triangle[i-1][j])
+        row.append(1)
+        P_triangle.append(row)
+    return [' '.join(str(num) for num in row) for row in P_triangle]
