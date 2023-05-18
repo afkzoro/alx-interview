38b837bbdac6f9888bca2e20182b2b42d3b9f3f3
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-18 13:29:45 +0100

    fix edge cases

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,31 +6,37 @@ validUTF8 method
 
 
 def validUTF8(data):
-    """
-     a method that determines if a given data set
+    """ a method that determines if a given data set
      represents a valid UTF-8 encoding
-    """
-    # Number of bytes for the current character
-    number_bytes = 0
+     """
+    num_continuation_bytes = 0
 
-    for i in data:
-        # Check if the current byte is a continuation byte
-        if number_bytes == 0:
-            if (i >> 7) == 0b0:
-                continue
-            elif (i >> 5) == 0b110:
-                number_bytes = 1
-            elif (i >> 4) == 0b1110:
-                number_bytes = 2
-            elif (i >> 3) == 0b11110:
-                number_bytes = 3
+    for byte in data:
+        if (byte >> 6) == 0b10:
+            if num_continuation_bytes == 0:
+                return False
+            num_continuation_bytes -= 1
+        else:
+            if num_continuation_bytes > 0:
+                return False
+            if (byte >> 7) == 0b0:
+                num_continuation_bytes = 0
+            elif (byte >> 5) == 0b110:
+                num_continuation_bytes = 1
+                if byte >> 4 == 0b1100:
+                    return False
+            elif (byte >> 4) == 0b1110:
+                num_continuation_bytes = 2
+                if byte >> 3 == 0b11100:
+                    return False
+            elif (byte >> 3) == 0b11110:
+                num_continuation_bytes = 3
+                if byte >> 2 == 0b111100:
+                    return False
             else:
                 return False
-        else:
-            # Check if the current byte is a valid continuation byte
-            if (i >> 6) != 0b10:
-                return False
 
-        number_bytes -= 1
+    if num_continuation_bytes > 0:
+        return False
 
-    return number_bytes == 0
+    return True
