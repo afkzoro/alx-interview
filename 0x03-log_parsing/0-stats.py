3280da17ae2d8676050f209188f02a5229fc2150
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-18 14:16:24 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,104 +1,52 @@
 #!/usr/bin/python3
-"""
-Metrics Computation Module
 
-This module reads log lines from stdin, computes various metrics,
-and prints the statistics based on the log lines.
-
-The log lines are expected to have the following format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-
-"""
 import sys
-import re
-from typing import TextIO
 
 
-def compute_metrics(input_stream: TextIO) -> None:  # noqa
-    """ Compute Metrics
-
-    Reads log lines from stdin, computes the total file size,
-    and counts the occurrences of each status code.
-
-    Prints the statistics after every 10 lines and the final statistics
-    when the program stops or encounters an exception.
+def print_msg(dict_sc, total_file_size):
+    """_summary_
 
     Args:
-        input_stream (log): logs generated
+        dict_sc (_type_): _description_
+        total_file_size (_type_): _description_
     """
 
-    # Initialize variables
-    total_size = 0
-    status_counts = {}
-    line_number = 0
-
-    # Regular expression pattern for matching the desired input format
-    reg = r'^(\S+) - \[(.+)\] "GET \/projects\/\d+ HTTP\/\d.\d" (\d+) (\d+)$'
-
-    try:
-        """sumary_line
-
-        Keyword arguments:
-        argument -- description
-        Return: return_description
-        """
-
-        # Process each line from stdin
-        for line in input_stream:
-            line = line.strip()
-
-            # Check if the line matches the desired format
-            match = re.match(reg, line)
-            if not match:
-                continue
-
-            # Parse the matched groups from the line
-            ip_address, date, stat_code, file_size = match.groups()
-
-            # Update total file size
-            try:
-                """_summary_
-    """
-                file_size = int(file_size)
-                total_size += file_size
-            except ValueError:
-                """_summary_
-    """
-                continue
-
-            # Update status code counts
-            try:
-                """_summary_
-    """
-                stat_code = int(stat_code)
-                status_counts[stat_code] = status_counts.get(stat_code, 0) + 1
-            except ValueError:
-                """_summary_
-    """
-                continue
-
-            line_number += 1
-
-            # Print statistics after every 10 lines
-            if line_number % 10 == 0:
-                print("File size:", total_size)
-                for code in sorted(status_counts.keys()):
-                    print(f"{code}: {status_counts[code]}")
-                # print()
-
-    except KeyboardInterrupt:
-        """_summary_
-    """
-        #   Handle keyboard interruption (CTRL + C)
-        #  print("File size:", total_size)
-        #  for code in sorted(status_counts.keys()):
-        #     print(f"{code}: {status_counts[code]}")
-        sys.exit(0)
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
-# Check if the module is run as a standalone script
-if __name__ == '__main__':
-    """_summary_
-    """
-    # Call the function to start computing metrics
-    compute_metrics(sys.stdin)
+file_size_total = 0
+code = 0
+count = 0
+dict_status_code = {"200": 0,
+                    "301": 0,
+                    "400": 0,
+                    "401": 0,
+                    "403": 0,
+                    "404": 0,
+                    "405": 0,
+                    "500": 0}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
+            count += 1
+
+            if count <= 10:
+                file_size_total += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_status_code.keys()):
+                    dict_status_code[code] += 1
+
+            if (count == 10):
+                print_msg(dict_status_code, file_size_total)
+                count = 0
+
+finally:
+    print_msg(dict_status_code, file_size_total)
