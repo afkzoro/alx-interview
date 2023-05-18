645746179b214b26c999f6ec597c553c0c234f0f
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-18 13:48:01 +0100

    mains

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -5,5 +5,5 @@ Main file for testing
 
 validUTF8 = __import__('0-validate_utf8').validUTF8
 
-data = [0b11100000, 0b10000000, 0b10000000]
+data = [0b11110100, 0b10000000, 0b10000000, 0b10000000]
 print(validUTF8(data))
