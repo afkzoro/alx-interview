ff19a31f06c68cbc7cc8c476047a4da0c57101e0
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-28 08:46:37 +0100

    fix PEP8 validation

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
 import sys
 
 
 def is_safe(board, row, col):
     # Check if a queen can be placed at board[row][col]
 
+    # Check row on the left side
     for i in range(col):
         if board[i] == row or board[i] == row + col - i or board[i] == row - col + i:
             return False
@@ -12,33 +12,32 @@ def is_safe(board, row, col):
     return True
 
 
-def solve_nqueens(N):
-    board = [-1] * N
+def solve_nqueens(n):
+    board = [-1] * n
     solutions = []
-    solve_util(board, 0, N, solutions)
+    solve_util(board, 0, n, solutions)
     return solutions
 
 
-def solve_util(board, col, N, solutions):
+def solve_util(board, col, n, solutions):
     # Base case: all queens are placed
-    if col == N:
+    if col == n:
         solutions.append(board[:])
         return
 
-    for row in range(N):
+    for row in range(n):
         if is_safe(board, row, col):
             # Place the queen
             board[col] = row
 
             # Recur for the next column
-            solve_util(board, col + 1, N, solutions)
+            solve_util(board, col + 1, n, solutions)
 
 
 def print_solutions(solutions):
     for solution in solutions:
         solution_list = [[row, solution[row]] for row in range(len(solution))]
         print(solution_list)
-    # print()
 
 
 def main():
@@ -48,17 +47,17 @@ def main():
         sys.exit(1)
 
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
     # Check if N is within the valid range
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    solutions = solve_nqueens(N)
+    solutions = solve_nqueens(n)
     print_solutions(solutions)
 
 
