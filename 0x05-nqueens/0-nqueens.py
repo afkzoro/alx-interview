662ac23557b70dd884df31e9292036e14b978f02
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-28 08:54:06 +0100

    fix PEP* validation

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,45 @@
-#!/usr/bin/python3
+#!/ usr / bin / python3
 import sys
 
 
 def is_safe(board, row, col):
     # Check if a queen can be placed at board[row][col]
 
-    # Check row on the left side
     for i in range(col):
-        if board[i] == row or board[i] == row + col - i or board[i] == row - col + i:
+        if (board[i] == row or board[i] == row + col - i
+                or board[i] == row - col + i):
             return False
 
     return True
 
 
-def solve_nqueens(n):
-    board = [-1] * n
+def solve_nqueens(N):
+    board = [ - 1 ] * N
     solutions = []
-    solve_util(board, 0, n, solutions)
+    solve_util(board, 0, N, solutions)
     return solutions
 
 
-def solve_util(board, col, n, solutions):
+def solve_util(board, col, N, solutions):
     # Base case: all queens are placed
-    if col == n:
+    if col == N:
         solutions.append(board[:])
         return
 
-    for row in range(n):
+    for row in range(N):
         if is_safe(board, row, col):
             # Place the queen
             board[col] = row
 
             # Recur for the next column
-            solve_util(board, col + 1, n, solutions)
+            solve_util(board, col + 1, N, solutions)
 
 
 def print_solutions(solutions):
     for solution in solutions:
         solution_list = [[row, solution[row]] for row in range(len(solution))]
         print(solution_list)
+    # print()
 
 
 def main():
@@ -48,17 +49,17 @@ def main():
         sys.exit(1)
 
     try:
-        n = int(sys.argv[1])
+        N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
     # Check if N is within the valid range
-    if n < 4:
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    solutions = solve_nqueens(n)
+    solutions = solve_nqueens(N)
     print_solutions(solutions)
 
 
