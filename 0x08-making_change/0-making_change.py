6f1c7e9b5a760397ef13a9d05d8fabc569076316
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-16 11:37:29 +0100

    improve code efficiency with sort algo

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,22 +11,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize an dynamic programming array
-    # to store the fewest number of coins needed for each total
-
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    # Iterate through all possible totals from 1 to 'total'
+    coins.sort(reverse=True)  # Sort coins in descending order
+
     for i in range(1, total + 1):
-        # Iterate through all available coin denominations
         for coin in coins:
-            if i - coin >= 0:
-
-                # Update the fewest number of coins needed
-                # if using this coin leads to a smaller value
+            if coin <= i:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
+            else:
+                break  # No need to consider smaller coins since they won't fit
 
-    # If the fewest number of coins for the total is still infinity
-    # it means the total cannot be met
     return dp[total] if dp[total] != float('inf') else -1
