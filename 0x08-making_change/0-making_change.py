4896845310bfdcc0b62064a37a358d868e645515
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-07-11 10:50:17 +0100

    changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Fewest Coins """
-import time
 
 
 def makeChange(coins, total):
@@ -8,28 +7,17 @@ def makeChange(coins, total):
     summary: Calculates the fewest number of coins
              needed for a given amount total.
     """
-
     if total <= 0:
         return 0
-
-    # Initialize an dynamic programming array
-    # to store the fewest number of coins needed for each total
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    # Iterate through all possible totals from 1 to 'total'
-    delay = 0.01
-    for i in range(1, total + 1):
-        # Iterate through all available coin denominations
-        for coin in coins:
-            if i - coin >= 0:
-
-                # Update the fewest number of coins needed
-                # if using this coin leads to a smaller value
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-                time.sleep(delay)
-
-    # If the fewest number of coins for the total is still infinity
-    # it means the total cannot be met
-    return dp[total] if dp[total] != float('inf') else -1
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
